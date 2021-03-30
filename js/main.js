581acba19e40b234334bc7ee5fb04fd35ca4cd6c
Vadym Koshelev <ardunk038@gmail.com>
2021-03-31 02:18:22 +0300

    Added autoplay feature

--- js/main.js
+++ js/main.js
@@ -1,30 +1,84 @@
 $(function() {
-  //init
-  $(document).ready(function() {
-    changeBanner('watchPink');
-    $('#carousel').owlCarousel({
-      dots: false,
-      rtl: true,
-      margin: 40,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        667: {
-          items: 2,
-          margin: 20,
-        },
-        1280: {
-          items: 1,
-        },
-        1600: {
-          items: 2,
-        }
+  //initialize carousel
+  $('#carousel').owlCarousel({
+    dots: false,
+    rtl: true,
+    margin: 40,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      667: {
+        items: 2,
+        margin: 20,
+      },
+      1280: {
+        items: 1,
+      },
+      1600: {
+        items: 2,
       }
-    });
+    }
   });
 
-  //show sidebar menu
+  //collection of banner images
+  let bannerImg = {
+    'watch-pink': 'url(./images/watch-pink.png)',
+    'watch-white': 'url(./images/watch-white.png)',
+    'watch-black': 'url(./images/watch-black.png)',
+  };
+  let bannerImgKeys = Object.keys(bannerImg);
+  let bannerAutoplay;
+
+  //load banner only when container is visible
+  if ($(window).width() >= 567) loadBanner();
+
+  //banner behavior on resize
+  $(window).resize(function() {
+    if ($(window).width() >= 567){
+      if (!$('#banner').attr('class')) {
+        loadBanner();
+      } else if (!bannerAutoplay) {
+        resetBannerAutoplay();
+      }
+    } else if (bannerAutoplay) {
+      bannerAutoplay = clearInterval(bannerAutoplay);
+    }
+  });
+
+  function loadBanner() {
+    changeBanner(bannerImgKeys[0]);
+    resetBannerAutoplay();
+  }
+
+  function resetBannerAutoplay() {
+    bannerAutoplay = clearInterval(bannerAutoplay);
+    bannerAutoplay = setInterval(() => swapWatch($('#banner').attr('class')), 5000);
+  }
+
+  // get next banner image
+  function swapWatch(currentItem) {
+    let nextIndex = bannerImgKeys.indexOf(currentItem) < bannerImgKeys.length - 1 ?
+      bannerImgKeys.indexOf(currentItem) + 1 :
+      0;
+    changeBanner(bannerImgKeys[nextIndex]);
+  };
+
+  //show new banner image
+  function changeBanner(newItem) {
+    $('#banner').removeClass().addClass(newItem);
+    $('#banner').css('background-image', bannerImg[newItem]);
+    if (($('#banner').is('.watch-black') && $('#wrapper').is('.dark')) ||
+      ($('#banner').is('.watch-white') && !$('#wrapper').is('.dark'))) {
+      toggleDarkMode();
+    }
+  };
+
+  function toggleDarkMode() {
+    $('#wrapper').toggleClass('dark');
+  };
+
+  //toggle sidebar menu
   $('#header-layout-button').click(function() {
     $('#wrapper').addClass('sidebar');
     $('#sidebar').addClass('active');
@@ -38,40 +92,12 @@ $(function() {
   //horizontal arrow click
   $('#model-nav').click(function() {
     swapWatch($('#banner').attr('class'));
+    resetBannerAutoplay();
   });
 
   //sync carousel item with banner
   $('.carousel-item').click(function() {
     changeBanner($(this).attr('id'));
+    resetBannerAutoplay();
   });
-
-  // change banner image
-  function swapWatch(currentItem) {
-    let nextIndex = bannerImgKeys.indexOf(currentItem) !== bannerImgKeys.length - 1 ?
-      bannerImgKeys.indexOf(currentItem) + 1 :
-      0;
-    changeBanner(bannerImgKeys[nextIndex]);
-  };
-
-  function changeBanner(newItem) {
-    $('#banner').removeClass().addClass(newItem);
-    $('#banner').css('background-image', bannerImg[newItem]);
-    if (($('#banner').is('.watchBlack') && $('#wrapper').is('.dark')) ||
-      ($('#banner').is('.watchWhite') && !$('#wrapper').is('.dark'))) {
-      toggleDarkMode();
-    }
-  };
-
-  //dark mode for site
-  function toggleDarkMode() {
-    $('#wrapper').toggleClass('dark');
-  };
-
-  //collection of banner images
-  let bannerImg = {
-    watchPink: 'url(./images/watch-pink.png)',
-    watchWhite: 'url(./images/watch-white.png)',
-    watchBlack: 'url(./images/watch-black.png)',
-  };
-  let bannerImgKeys = Object.keys(bannerImg);
 });
